bc177657d402397ec1b960ef663437550857907e
GreyTH <68487068+GreyTH@users.noreply.github.com>
2022-07-11 17:45:46 -0600

    Add files via upload

---
+++ Cafeteria/src/index.php
@@ -0,0 +1,352 @@
+
+
+   <php?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"  crossorigin="anonymous"> 
+    <title>Cafetería</title>
+    <link rel="shortcut icon" href="./Imagenes/Inicio/Inicio.png" type="image/x-icon">
+    <style type="text/css">
+        img {
+            width: 155px;
+            height: auto;
+        }
+    </style>
+    <link rel="stylesheet" href="./CSS/Inicio.Css" type="text/css"> 
+    <link rel="stylesheet" href="../Css/estilo.css">
+    <link rel="stylesheet" href="../Css/Inicio.css">
+</head>
+
+<body>
+
+<header>
+<br>
+            <br>
+        <img src="../img/logo.png" alt="Logo">
+        <br>
+            <br>
+            <br>
+            <br>
+        <nav>
+           
+            <a href="./src/index.php">Inicio</a>
+            <a href="./src/Catalago.php">Catalago</a>
+            <a href="./src/Facturacion.php">    Factura</a>
+            <a href="./src/Reporte.php">  Reportes</a>
+            <a href="./src/Contactanos.php">Contactanos</a>
+           <a href="./src/Galeria.php">Galeria</a>
+
+            <div class="animation start-home"></div>
+        </nav>
+
+        <section class="textos-header" class="title-cards">
+            <h1 style = "font-family: times">¡CAFETERIA EL RINCON DEL BUEN CAFE.!</h1>
+
+        </section>
+
+        <div class="wave" style="height: 150px; overflow: hidden;">
+            <svg viewBox="0 0 500 150" preserveAspectRatio="none" style="height: 100%; width: 100%;">
+             <path d="M0.00,49.98 C150.00,150.00 349.20,-50.00 500.00,49.98 L500.00,150.00 L0.00,150.00 Z" style="stroke: none; "></path>
+            </svg>
+        </div>
+    </header>
+
+
+
+
+
+
+
+	<div class="title-cards">
+		<h2 style = "font-family: times ">¿QUÉ SE TE ANTOJA HOY?</h2>
+	</div>
+	<div class="container-card">
+	
+	<div class="card">
+	<figure>
+		<img src="../img/latte.jpg">
+	</figure>
+	<div class="contenido-card">
+		<h3 style = "font-family: times ">Latte</h3>
+		<p> Disfruta del mejor Latte, los mejores sabores a tu alcance.
+			No pierdas mas tiempo para porbar de nuestros delicioso latte, elborados con los
+			mejores ingredientes.
+		</p>
+		<a href="#">Leer Màs</a>
+	</div>
+	</div>
+	
+	<div class="card">
+	<figure>
+		<img src="../img/marmoleado.jpg">
+	</figure>
+	<div class="contenido-card">
+		<h3 style = "font-family: times ">Marmoleado</h3>
+		<p>El mejor pan marmoleado, echo con los mejores ingredientes para que disfrutes solo o acompañado.
+			Las mejores opciones las encuentras en nuestra cafetería.
+		</p>
+		<a href="#">Leer Màs</a>
+	</div>
+	</div>
+	<div class="card">
+	<figure>
+		<img src="../img/chocolate.jpeg">
+	</figure>
+	<div class="contenido-card">
+		<h3 style = "font-family: times ">Chocolate Caliente</h3>
+		<p>Disfruta del mejor chocolate caliente, eleborado con pasion para que disfrutes de los mejores sabores.
+			Nuestra especialidad es ofrecerte los mejores productos.
+		</p>
+		<a href="#">Leer Màs</a>
+	</div>
+	</div>
+	</div>
+
+
+
+<br>
+
+	<div class="card-group">
+		<div class="card bg-primary">
+		  <div class="card-body text-center">
+			<p class="card-text">Historia y Tradición</p>
+		  </div>
+		</div>
+		<div class="card bg-warning">
+		  <div class="card-body text-center">
+			<p class="card-text">Café 100% original</p>
+		  </div>
+		</div>
+		<div class="card bg-success">
+		  <div class="card-body text-center">
+			<p class="card-text">Adquirido en su punto de perfección</p>
+		  </div>
+		</div>
+		<div class="card bg-danger">
+		  <div class="card-body text-center">
+			<p class="card-text">Calidad total</p>
+		  </div>
+		</div>
+	  </div>
+
+<br>
+
+<div class="title-cards">
+	<h2 style = "font-family: times ">LOS MEJORES SABORES PARA CADA TEMPORADA</h2>
+</div>
+<div class="cards-liste">
+  
+	<div class="cardes 1">
+	  <div class="card_image"> 
+		  <img src="https://c.wallhere.com/photos/5e/a0/life_guangzhou_blue_summer_food_closeup_canon_eos-883015.jpg!d" /> </div>
+	  <div class="card_title title-white">
+		<p></p>
+	  </div>
+	</div>
+	
+	  <div class="cardes 2">
+	  <div class="card_image">
+		<img src="https://recetasabc.com/wp-content/uploads/2017/11/coffee-1974841_640.jpg" />
+		</div>
+	  <div class="card_title title-white">
+		<p></p>
+	  </div>
+	</div>
+	
+	<div class="cardes 3">
+	  <div class="card_image">
+		<img src="https://cdn2.cocinadelirante.com/sites/default/files/images/2020/04/recetas-de-bebidas-frias-para-cafeteria-caseras.jpg" />
+	  </div>
+	  <div class="card_title">
+		<p></p>
+	  </div>
+	</div>
+	  
+	  <div class="cardes 4">
+	  <div class="card_image">
+		<img src="https://st1.uvnimg.com/dims4/default/4d3bc14/2147483647/thumbnail/480x270/quality/75/?url=https%3A%2F%2Fuvn-brightspot.s3.amazonaws.com%2Fassets%2Fvixes%2Fimj%2Fvivirsalud%2F7%2F7-razones-para-beber-mas-cafe-3.jpg" />
+		</div>
+	  <div class="card_title title-black">
+		<p></p>
+	  </div>
+	  </div>
+	</div>
+
+
+
+
+
+	<section>
+		<div id="container">	
+		<!-- Start	Product details -->
+			<div class="product-details">
+			<p style = "font-family: times "><h1>HISTORIA</h1> </p>
+			<p class="information">"El Rincón del buen café comienza
+				en el 1997 cuando la pirmera tienda se abrio en Santa Barbara.
+				Nuestra cafeteria inicialmente solo vendia bolsas de café. 
+				De pronto surgio la idea de hacer y afrecer productos de café, ya sea
+				para disfrutar en el local, como para llevar a tu casa."</p>
+		
+				
+			
+		</div>
+		<div class="product-image">	
+			<img src="../img/cafett.jpg" alt="Omar Dsoky">	
+		<div class="info">
+			<h2>Descripción</h2>
+			<br>
+			<ul>
+				<li><strong>Cafetería: </strong>El Rincon del Buen Cafe</li>
+				<br>
+				<li><strong>Año de fundacion: </strong>1997</li>
+				<br>
+				<li><strong>Dueño: </strong>Fabricio Sagastume</li>
+			</ul>
+		</div>
+		</div>
+		</div>
+		</section>
+
+	  <br>
+	  <br>
+
+	<div class="row">
+		<div class="col-sm-6">
+		  <div class="card">
+			<div class="card-body">
+			  <center><h5 class="card-title" style = "font-family: times ">MISION</h5> </center>
+			  <p class="card-text" >Nuestra mision es satisfacer plenamente las necesidades de 
+				  nuestros clientes y finalmente sorprenderlos en sus expectativas,
+				brindandoles un excelente servicio y haciendo que la cafeteria este cada vez mas
+			extendida y popularizada. </p>
+			</div>
+		  </div>
+		</div>
+		
+		<br>
+		<br>
+		<div class="col-sm-6">
+		  <div class="card">
+			<div class="card-body">
+			<center><h5 class="card-title" style = "font-family: times ">VISION</h5> </center>
+			  <p class="card-text">Ser una empresa estrictamente profecional y humana en la prestacion
+				  de servicios de cafe, con un compromiso a largo plazo con sus clientes,
+				  socios y accionistas.
+			  </p>
+			</div>
+		  </div>
+		</div>
+	  </div>
+
+
+	  <br>
+	  <br><br>
+	  <br>
+
+	  <font COLOR="white"><center> <h2 style = "font-family: times ">Valores</h2></center></font>
+	  <div class="row1-container">
+		<div class="box box-down cyan">
+			<font COLOR="white"><h2 style = "font-family: times ">Calidad</h2></font>
+		  <p> Ofrecer el mejor café con altos estándares de calidad, que cumpla con las expectativas del cliente.</p>
+		  <img src="https://cdn-icons-png.flaticon.com/128/2620/2620470.png" alt="">
+		</div>
+	
+		<div class="box red">
+			<font COLOR="white"> <h2 style = "font-family: times ">Pasion</h2></font>
+		  <p>Estar siempre dispuesto a dar lo mejor de uno mismo, es un requisito indispensable para conseguir 
+			la mejor experiencia de nuestros clientes</p>
+		  <img src="https://cdn-icons-png.flaticon.com/128/4264/4264354.png" alt="">
+		</div>
+	
+		<div class="box box-down blue">
+			<font COLOR="white"> <h2 style = "font-family: times ">Responsabilidad</h2></font>
+		  <p>Cumplimos con los compromisos y expectativas en tiempo y forma, prevemos y asumimos las consecuencias
+			de nuestras decisiones, rendimos cuentas y actuamos conforme a los procesos establecidos.</p>
+		  <img src="https://cdn-icons-png.flaticon.com/128/2191/2191159.png" alt="">
+		</div>
+	  </div>
+	  <div class="row2-container">
+		<div class="box orange">
+			<font COLOR="white"><h2 style = "font-family: times ">Colaboracion</h2></font>
+		  <p>Disposición para ayudar, compartir y trabajar tareas en común, que se llevan a cabo por un grupo de colaboradores, 
+			sin intención de competir por sus intereses sino por cumplir un mismo objetivo</p>
+		  <img src="https://cdn-icons-png.flaticon.com/128/1147/1147955.png" alt="">
+		</div>
+	  </div>	
+	 
+
+
+	  <br>
+	  <br>
+
+
+
+
+
+
+
+<br>
+<br>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.min.js" integrity="sha384-VHvPCCyXqtD5DqJeNxl2dtTyhF78xXNXdkwX1CZeRusQfRKp+tA7hAShOK/B/fQ2" crossorigin="anonymous"></script>
+	
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700&display=swap" rel="stylesheet">
+	<script src="https://kit.fontawesome.com/2c36e9b7b1.js"></script>
+
+
+	<script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>	
+
+
+
+
+
+<!-- Remove the container if you want to extend the Footer to full width.    en footer class antes de text bg-dark-->
+<div class="container my-5">
+
+  <footer class=" text-center text-white">
+  <!-- Grid container -->
+  <div class="container p-4 pb-0">
+    <!-- Section: Social media -->
+    <section class="mb-4">
+      <!-- Facebook -->
+      <a style="background-color: #3b5998;" class="btn btn-outline-light btn-floating m-1" href="https://es-la.facebook.com/" role="button"
+        ><i class="fab fa-facebook-f"></i
+      ></a>
+
+      <!-- Twitter Style le da color al icono de la aplicacion -->
+      <a class="btn btn-outline-light btn-floating m-1" style="background-color: #55acee;" href="https://twitter.com/i/flow/login" role="button"
+        ><i class="fab fa-twitter"></i
+      ></a> 
+
+      <!-- Google -->
+      <a class="btn btn-outline-light btn-floating m-1" style="background-color: #dd4b39;" href="https://www.google.com/webhp?hl=es&sa=X&ved=0ahUKEwjEuYXTjqb4AhUhTTABHffBA38QPAgI" role="button"
+        ><i class="fab fa-google"></i
+      ></a>
+
+      <!-- Instagram -->
+      <a class="btn btn-outline-light btn-floating m-1"  style="background-color: #ac2bac;" href="https://www.instagram.com/" role="button"
+        ><i class="fab fa-instagram"></i
+      ></a>
+    </section>
+
+  </div>
+
+  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+    © 2022 Gestion De Proyectos Informaticos:
+    <a class="text-white" href="https://login.sec.unicah.net/pages/login.php">Grupo #4</a>
+  </div>
+  <!-- Copyright -->
+</footer>
+  
+</div>
+
+
+    </body>
+
+    </php>
+
+
